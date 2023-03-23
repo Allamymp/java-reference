ec051c56b48a2cdaf3051d6efe9b9f86828ec645
allamymp <allamympereira@gmail.com>
2023-03-23 05:37:00 -0300

    class User in package set created

--- src/set/Exercise1.java
+++ src/set/Exercise1.java
@@ -1,19 +1,14 @@
 package set;
 
+
+
 public class Exercise1 {
 
 	public static void main(String[] args) {
 		
-		
-		try {
-			
-			
-			
-		}
-		catch{
-			
-		}
-
+ 
+		 
+		 
 	}
 
 }
--- src/set/User.java
+++ src/set/User.java
@@ -1,5 +1,32 @@
 package set;
 
+import java.time.LocalDateTime;
+
 public class User {
+
+	private String name;
+	private LocalDateTime acessTime;
+
+	public User(String name, LocalDateTime acessTime) {
+
+		this.name = name;
+		this.acessTime = acessTime;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getAcessTime() {
+		return acessTime;
+	}
+
+	public void setAcessTime(LocalDateTime acessTime) {
+		this.acessTime = acessTime;
+	}
 
 }
