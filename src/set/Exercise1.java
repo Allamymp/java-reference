e054fff86263935cc87c33a3fbe7f6687f2aa58a
allamymp <allamympereira@gmail.com>
2023-03-23 06:17:04 -0300

    class User updated

--- src/set/Exercise1.java
+++ src/set/Exercise1.java
@@ -6,9 +6,7 @@ public class Exercise1 {
 
 	public static void main(String[] args) {
 		
- 
-		 
-		 
+		
 	}
 
 }
--- src/set/User.java
+++ src/set/User.java
@@ -1,16 +1,20 @@
 package set;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class User {
 
 	private String name;
 	private LocalDateTime acessTime;
+	private String formatedDate;
 
 	public User(String name, LocalDateTime acessTime) {
 
 		this.name = name;
 		this.acessTime = acessTime;
+		dateTimeFormatter(this.acessTime);
 	}
 
 	public String getName() {
@@ -27,6 +31,15 @@ public class User {
 
 	public void setAcessTime(LocalDateTime acessTime) {
 		this.acessTime = acessTime;
+	}
+
+	public void dateTimeFormatter(LocalDateTime acessTime) {
+		  DateTimeFormatter isoFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		this.formatedDate = isoFormatter.format(acessTime);
+	}
+
+	public String getFormatedDate() {
+		return formatedDate;
 	}
 
 }
